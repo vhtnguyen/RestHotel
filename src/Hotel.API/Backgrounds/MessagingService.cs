d20cb55d3910b41527cc4e6f8ce52731351301d8
20120551 <killermh1352@gmail.com>
2023-05-17 02:39:54 +0700

    fix git bug

--- src/Hotel.API/Backgrounds/MessagingService.cs
+++ src/Hotel.API/Backgrounds/MessagingService.cs
@@ -1,14 +1,32 @@
-﻿
-private async Task consumeAsync(int worker)
+﻿using Hotel.Shared.Handlers;
+using Hotel.Shared.Messaging;
+
+namespace Hotel.API.Backgrounds
 {
-    while (true)
+    public class MessagingService : BackgroundService
     {
-        if (await messagingChannel.Reader.WaitToReadAsync())
+        private readonly IMessagingChannel<ICommand> messagingChannel;
+
+        public MessagingService(IMessagingChannel<ICommand> messagingChannel)
         {
-            var value = await messagingChannel.Reader.ReadAsync();
-            Console.WriteLine($"worker {worker}");
+            this.messagingChannel = messagingChannel;
         }
-    }
-}
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // 2 worker
+            return Task.Run(() => Task.WaitAll(consumeAsync(1), consumeAsync(2), consumeAsync(3)));
+        }
+
+        private async Task consumeAsync(int worker)
+        {
+            while (true)
+            {
+                if (await messagingChannel.Reader.WaitToReadAsync())
+                {
+                    var value = await messagingChannel.Reader.ReadAsync();
+                    Console.WriteLine($"worker {worker}");
+                }
+            }
+        }
     }
 }
--- src/Hotel.API/Backgrounds/StreamingService.cs
+++ src/Hotel.API/Backgrounds/StreamingService.cs
@@ -1,19 +1,27 @@
-﻿
-public StreamingService(
-    IStreamingPublisher streamingPublisher)
+﻿using Hotel.BussinessLogic.Commands;
+using Hotel.Shared.Redis;
+
+namespace Hotel.API.Backgrounds
 {
-    this.streamingPublisher = streamingPublisher;
-}
-protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-{
-    while (true)
+    public class StreamingService : BackgroundService
     {
-        var command = new SendNotificationCommand();
-        await streamingPublisher.PublishAsync("email", command);
-        await streamingPublisher.PublishAsync("email", command);
-        await streamingPublisher.PublishAsync("email", command);
-        await Task.Delay(1000);
-    }
-}
+        private readonly IStreamingPublisher streamingPublisher;
+
+        public StreamingService(
+            IStreamingPublisher streamingPublisher)
+        {
+            this.streamingPublisher = streamingPublisher;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                var command = new SendNotificationCommand();
+                await streamingPublisher.PublishAsync("email", command);
+                await streamingPublisher.PublishAsync("email", command);
+                await streamingPublisher.PublishAsync("email", command);
+                await Task.Delay(1000);
+            }
+        }
     }
 }
