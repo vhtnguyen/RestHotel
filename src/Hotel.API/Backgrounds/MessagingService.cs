43af4f03c3d0d7c2ce34c6bdb75e09cfdec06095
20120551 <killermh1352@gmail.com>
2023-05-17 01:45:33 +0700

    Add async migrations

---
+++ src/Hotel.API/Backgrounds/MessagingService.cs
@@ -0,0 +1,32 @@
+﻿using Hotel.Shared.Handlers;
+using Hotel.Shared.Messaging;
+
+namespace Hotel.API.Backgrounds
+{
+    public class MessagingService : BackgroundService
+    {
+        private readonly IMessagingChannel<ICommand> messagingChannel;
+
+        public MessagingService(IMessagingChannel<ICommand> messagingChannel)
+        {
+            this.messagingChannel = messagingChannel;
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // 2 worker
+            return Task.Run(() => Task.WaitAll(consumeAsync(1), consumeAsync(2), consumeAsync(3)));
+        }
+
+        private async Task consumeAsync(int worker)
+        {
+            while(true)
+            {
+                if(await messagingChannel.Reader.WaitToReadAsync())
+                {
+                    var value = await messagingChannel.Reader.ReadAsync();
+                    Console.WriteLine($"worker {worker}");
+                }
+            }
+        }
+    }
+}
---
+++ src/Hotel.API/Backgrounds/StreamingService.cs
@@ -0,0 +1,30 @@
+﻿using Hotel.BussinessLogic.Commands;
+using Hotel.Shared.Redis;
+
+namespace Hotel.API.Backgrounds
+{
+    public class StreamingService : BackgroundService
+    {
+        private readonly IStreamingPublisher streamingPublisher;
+        private readonly IStreamingSubscriber streamingSubscriber;
+
+        public StreamingService(
+            IStreamingPublisher streamingPublisher,
+            IStreamingSubscriber streamingSubscriber)
+        {
+            this.streamingPublisher = streamingPublisher;
+            this.streamingSubscriber = streamingSubscriber;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(true)
+            {
+                var command = new SendNotificationCommand();
+                await streamingPublisher.PublishAsync("email", command);
+                await streamingPublisher.PublishAsync("email", command);
+                await streamingPublisher.PublishAsync("email", command);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
